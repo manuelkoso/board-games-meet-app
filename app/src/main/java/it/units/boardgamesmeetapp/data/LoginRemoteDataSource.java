9a15b70a049f7ddecfbf100e346e82782704d636
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-07-24 21:03:24 +0200

    Implemented Firebase login

---
+++ app/src/main/java/it/units/boardgamesmeetapp/data/LoginRemoteDataSource.java
@@ -0,0 +1,33 @@
+package it.units.boardgamesmeetapp.data;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import it.units.boardgamesmeetapp.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginRemoteDataSource {
+
+    private FirebaseAuth firebaseAuth;
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            firebaseAuth = FirebaseAuth.getInstance();
+            if(firebaseAuth.signInWithEmailAndPassword(username,password).isSuccessful()) {
+                LoggedInUser currentUser = new LoggedInUser(firebaseAuth.getUid(), username);
+                return new Result.Success<>(currentUser);
+            }
+            throw new IOException("Error logging in");
+        } catch (Exception e) {
+            return new Result.Error(e);
+        }
+    }
+
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+    }
+}
