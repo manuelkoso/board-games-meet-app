0532f7fc049fa8f1f80c46e37a2ec8e9219e1dac
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-07-25 15:22:58 +0200

    Refactoring

--- app/src/main/java/it/units/boardgamesmeetapp/MainActivity.java
+++ app/src/main/java/it/units/boardgamesmeetapp/MainActivity.java
@@ -2,17 +2,10 @@ package it.units.boardgamesmeetapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
-
-import it.units.boardgamesmeetapp.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ActionOnlyNavDirections;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -20,23 +13,18 @@ import androidx.navigation.ui.NavigationUI;
 import java.util.Objects;
 
 import it.units.boardgamesmeetapp.databinding.ActivityMainBinding;
-import it.units.boardgamesmeetapp.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         hideUpperActionBar();
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_dashboard, R.id.navigation_home, R.id.navigation_notifications)
                 .build();
@@ -45,15 +33,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
 
         navController.addOnDestinationChangedListener(((controller, destination, arguments) -> {
-            if (destination.getId() == R.id.navigation_notifications|| destination.getId() == R.id.navigation_dashboard || destination.getId() == R.id.navigation_home) {
-                binding.navView.setVisibility(View.VISIBLE);
-                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            } else if (destination.getId() == R.id.loginFragment || destination.getId() == R.id.signupFragment) {
+            if (destination.getId() == R.id.loginFragment || destination.getId() == R.id.signupFragment) {
                 binding.navView.setVisibility(View.GONE);
-                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             } else {
                 binding.navView.setVisibility(View.VISIBLE);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             }
         }));
 
