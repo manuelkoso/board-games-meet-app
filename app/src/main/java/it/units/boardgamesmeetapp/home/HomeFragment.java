199a306c65413acb9839237d425c57ddcd012db0
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-08-12 12:39:37 +0200

    Added button listener

--- app/src/main/java/it/units/boardgamesmeetapp/home/HomeFragment.java
+++ app/src/main/java/it/units/boardgamesmeetapp/home/HomeFragment.java
@@ -4,29 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.ActionOnlyNavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.sql.Ref;
+
+import it.units.boardgamesmeetapp.R;
 import it.units.boardgamesmeetapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private HomeViewModel homeViewModel;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         final TextView textView = binding.textHome;
+        final FloatingActionButton newActivityButton = binding.newActivityButton;
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+        newActivityButton.setOnClickListener(v -> {
+            NavHostFragment.findNavController(this).navigate(new ActionOnlyNavDirections(R.id.action_navigation_home_to_addNewActivityFragment));
+        });
+
     }
 
     @Override
