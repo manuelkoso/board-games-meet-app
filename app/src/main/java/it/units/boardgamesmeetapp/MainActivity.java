f5cfa1ee682542ac8f89124f396b8d28cc24edc2
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-08-22 15:21:31 +0200

    Name refactored and changed icon

--- app/src/main/java/it/units/boardgamesmeetapp/MainActivity.java
+++ app/src/main/java/it/units/boardgamesmeetapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         hideUpperActionBar();
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_dashboard, R.id.navigation_home, R.id.navigation_notifications)
+                R.id.navigation_dashboard, R.id.navigation_home, R.id.navigation_profile)
                 .build();
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
--- app/src/main/java/it/units/boardgamesmeetapp/dashboard/DashboardFragment.java
+++ app/src/main/java/it/units/boardgamesmeetapp/dashboard/DashboardFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,18 +16,19 @@ import it.units.boardgamesmeetapp.databinding.FragmentDashboardBinding;
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+
+    private DashboardViewModel viewModel;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        return binding.getRoot();
+    }
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
