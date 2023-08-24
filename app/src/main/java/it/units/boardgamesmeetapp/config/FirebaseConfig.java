1ff84bc8b87afd688c8a57d81ecabb2c035644dc
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-08-24 08:47:50 +0200

    Bug fixed

--- app/src/main/java/it/units/boardgamesmeetapp/config/FirebaseConfig.java
+++ app/src/main/java/it/units/boardgamesmeetapp/config/FirebaseConfig.java
@@ -5,7 +5,7 @@ public class FirebaseConfig {
     private FirebaseConfig(){}
     public static final String TAG = "FIREBASE";
     public static final String SUCCESSFUL_LOGIN_MESSAGE = "Successful login";
-    public static final String LOGIN_ERROR_MESSAGE = "Successful login";
+    public static final String LOGIN_ERROR_MESSAGE = "Login failure";
     public static final String DB_URL = "https://board-games-meet-app-default-rtdb.europe-west1.firebasedatabase.app";
 
 }
