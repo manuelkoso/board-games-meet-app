590efe78a59fdd7c1cb7a567c6377410bd36f489
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-09-09 15:00:12 +0200

    Refactoring

--- app/src/main/java/it/units/boardgamesmeetapp/profile/ProfileViewModel.java
+++ app/src/main/java/it/units/boardgamesmeetapp/profile/ProfileViewModel.java
@@ -1,32 +1,46 @@
 package it.units.boardgamesmeetapp.profile;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Objects;
+
+import it.units.boardgamesmeetapp.database.FirebaseConfig;
 import it.units.boardgamesmeetapp.models.User;
 import it.units.boardgamesmeetapp.models.UserInfo;
 
 public class ProfileViewModel extends ViewModel {
-
+    @NonNull
     private final FirebaseFirestore database;
+    @NonNull
     private final FirebaseAuth firebaseAuth;
     private final MutableLiveData<UserInfo> initialUserInfo = new MutableLiveData<>();
     private final MutableLiveData<UserInfo> currentUserInfo = new MutableLiveData<>();
 
-    public ProfileViewModel(FirebaseAuth firebaseAuth, FirebaseFirestore database) {
+    public ProfileViewModel(@NonNull FirebaseAuth firebaseAuth, @NonNull FirebaseFirestore database) {
         this.database = database;
         this.firebaseAuth = firebaseAuth;
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        DocumentReference reference = database.collection("users").document(firebaseUser.getUid());
+        DocumentReference reference = database.collection(FirebaseConfig.USERS).document(Objects.requireNonNull(firebaseAuth.getUid()));
         reference.addSnapshotListener(
                 (value, exception) -> {
-                    User user = value.toObject(User.class);
-                    initialUserInfo.setValue(user.getInfo());
+                    if (exception != null) {
+                        Log.w(FirebaseConfig.TAG, exception);
+                        return;
+                    }
+                    if (value != null && value.exists()) {
+                        Log.d(FirebaseConfig.TAG, "Data: " + value.getData());
+                        User user = value.toObject(User.class);
+                        initialUserInfo.setValue(Objects.requireNonNull(user).getInfo());
+                    } else {
+                        Log.w(FirebaseConfig.TAG, "Data: null");
+                    }
                 }
         );
     }
@@ -36,9 +50,14 @@ public class ProfileViewModel extends ViewModel {
     }
 
     public void modifyUserInformation() {
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        DocumentReference reference = database.collection("users").document(firebaseUser.getUid());
-        reference.update("info", currentUserInfo.getValue().toMap());
+        DocumentReference reference = database.collection(FirebaseConfig.USERS).document(Objects.requireNonNull(firebaseAuth.getUid()));
+        reference.update("info", Objects.requireNonNull(currentUserInfo.getValue()).toMap()).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Log.d(FirebaseConfig.TAG, "User information modified correctly");
+            } else {
+                Log.w(FirebaseConfig.TAG, task.getException());
+            }
+        });
     }
 
     public void updateCurrentInfoUser(UserInfo userInfo) {
