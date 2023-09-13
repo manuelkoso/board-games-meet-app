779efa0615e535e9f1a9ae4f4d1c283968f99199
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-09-13 22:22:01 +0200

    Refactor

--- app/src/main/java/it/units/boardgamesmeetapp/MainActivity.java
+++ app/src/main/java/it/units/boardgamesmeetapp/MainActivity.java
@@ -1,15 +1,11 @@
 package it.units.boardgamesmeetapp;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NavUtils;
-import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -18,18 +14,14 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
 import it.units.boardgamesmeetapp.databinding.ActivityMainBinding;
-import it.units.boardgamesmeetapp.fragments.HomeFragment;
 import it.units.boardgamesmeetapp.viewmodels.MainViewModel;
 import it.units.boardgamesmeetapp.viewmodels.MainViewModelFactory;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        viewModel = new ViewModelProvider(this, new MainViewModelFactory()).get(MainViewModel.class);
+        MainViewModel viewModel = new ViewModelProvider(this, new MainViewModelFactory()).get(MainViewModel.class);
         viewModel.getActionBarTitle().observe(this, Objects.requireNonNull(binding.topAppBar)::setTitle);
 
         NavHostFragment navHostFragment =
@@ -46,12 +38,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
 
         navController.addOnDestinationChangedListener(((controller, destination, arguments) -> {
+            Menu menu = binding.topAppBar.getMenu();
             if (destination.getId() == R.id.navigation_login || destination.getId() == R.id.navigation_signup) {
-                Menu menu = binding.topAppBar.getMenu();
                 menu.findItem(R.id.more).setVisible(false);
                 binding.navView.setVisibility(View.GONE);
             } else {
-                Menu menu = binding.topAppBar.getMenu();
                 menu.findItem(R.id.more).setVisible(true);
                 binding.navView.setVisibility(View.VISIBLE);
             }
@@ -59,13 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(binding.topAppBar).setOnMenuItemClickListener(item -> {
             if(item.getItemId() == R.id.more) {
-                new MaterialAlertDialogBuilder(this).setTitle("Logout")
-                        .setMessage("Do you want to logout?")
-                        .setPositiveButton("Yes", (dialogInterface, i) -> {
-                            viewModel.logout();
-                            navController.navigate(R.id.action_global_loginFragment);
-                        }).setNegativeButton("No", ((dialogInterface, i) -> {
-                        })).show();
+                buildLogoutDialog(viewModel, navController).show();
                 return true;
             }
             return false;
@@ -76,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             NavigationUI.setupWithNavController((NavigationView) binding.navView, navController);
         }
+    }
+
+    private AlertDialog buildLogoutDialog(MainViewModel viewModel, NavController navController) {
+        return new MaterialAlertDialogBuilder(this).setTitle(R.string.logout)
+                .setMessage(R.string.logout_question)
+                .setPositiveButton(R.string.yes, (dialogInterface, i) -> {
+                    viewModel.logout();
+                    navController.navigate(R.id.action_global_loginFragment);
+                }).setNegativeButton(R.string.no, ((dialogInterface, i) -> {
+                })).show();
     }
 
 }
